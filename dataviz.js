1a93092a4a5a66c68c1d41e2138af98e90847a91
Veli Gasparovic <velimir.gasparovic@gmail.com>
2024-11-19 12:27:48 +0100

    D3 VizPod predavanje

--- dataviz.js
+++ dataviz.js
@@ -4,10 +4,10 @@ const svg = d3.select(".chart");
 const width = svg.node().clientWidth;
 const height = svg.node().clientHeight;
 
-svg.style("background-color", "purple");
+// svg.style("background-color", "purple");
 
 //read the data form a local wordl_annual.json file
-d3.json("world_annual.json").then((data) => {
+await d3.json("world_annual.json").then(async (data) => {
   console.log(data);
   //   debugger;
 
@@ -21,10 +21,10 @@ d3.json("world_annual.json").then((data) => {
 
   console.log(data);
 
-  render(data);
+  await render(data);
 });
 
-function render(data) {
+async function render(data) {
   // create a scaleBand for the x axis, spanning full width of the chart
   const xScale = d3
     .scaleBand()
@@ -52,82 +52,94 @@ function render(data) {
 
   // console.log(d3.extent(data, (d) => d.anomaly));
 
-  // const maxAnomaly = d3.max(data, (d) => Math.abs(d.anomaly));
+  const maxAnomaly = d3.max(data, (d) => Math.abs(d.anomaly));
 
-  // // let's contruct a color scale for the rectangles, divergent (blue to red, white in the middle)
-  // const colorScale = d3.scaleDiverging(
-  //   [maxAnomaly, 0, -maxAnomaly],
-  //   // d3.interpolateRdBu
-  //   d3.interpolatePiYG
-  // );
+  // let's contruct a color scale for the rectangles, divergent (blue to red, white in the middle)
+  const colorScale = d3.scaleDiverging(
+    [maxAnomaly, 0, -maxAnomaly],
+    // d3.interpolateRdBu
+    d3.interpolatePiYG
+  );
 
-  // // let's iterate data and drawing full heigh rectangles:
-  // const rectGroup = svg.append("g");
-  // data.forEach((d, i) => {
-  //   const rect = rectGroup
-  //     .append("rect")
-  //     .attr("x", xScale(i))
-  //     .attr("y", 0)
-  //     .attr("width", xScale.bandwidth())
-  //     .attr("fill", "white")
-  //     // .attr("fill", colorScale(d.anomaly))
-  //     .attr("height", height);
+  // let's iterate data and drawing full heigh rectangles:
+  const rectGroup = svg.append("g");
 
-  //   rect
-  //     .transition()
-  //     .duration(1000)
-  //     .delay(20 * i)
-  //     .attr("fill", colorScale(d.anomaly));
+  const rects = data.map((d, i) => {
+    return rectGroup
+      .append("rect")
+      .attr("x", xScale(i))
+      .attr("y", 0)
+      .attr("width", xScale.bandwidth())
+      .attr("fill", "white")
+      .attr("height", height);
+  });
 
-  //   const offset = 30;
+  // Create and await all transitions
+  await Promise.all(
+    rects.map((rect, i) => {
+      const t = rect
+        .transition()
+        .duration(1000)
+        .delay(20 * i)
+        .attr("fill", colorScale(data[i].anomaly));
 
-  //   //   add a mouseover to transition the rect 10 pixels up and add a text showing the year and anomaly
-  //   rect.on("mouseover", () => {
-  //     // rect.transition().duration(250).attr("y", -offset);
-  //     rect.attr("stroke", "black").attr("stroke-width", 1).raise();
+      return t.end();
+    })
+  );
 
-  //     const tooltipGroup = svg
-  //       .append("g")
-  //       .attr("class", "tooltip")
-  //       .attr(
-  //         "transform",
-  //         `translate(${xScale(i) + xScale.bandwidth() + 4}, ${-offset})`
-  //       )
-  //       .style("pointer-events", "none")
-  //       .style("font-size", "11px")
-  //       .style("font-family", "sans-serif")
-  //       .style("dominant-baseline", "hanging");
+  // After all transitions complete, add the interactions
+  rects.forEach((rect, i) => {
+    const d = data[i];
+    const offset = 30;
 
-  //     tooltipGroup
-  //       .style("opacity", 0)
-  //       .transition()
-  //       .duration(250)
-  //       .style("opacity", 1);
+    rect.on("mouseover", () => {
+      rect.transition().duration(250).attr("y", -offset);
+      rect.attr("stroke", "black").attr("stroke-width", 1).raise();
 
-  //     tooltipGroup.append("text").text(`${d.year}.`);
+      const tooltipGroup = svg
+        .append("g")
+        .attr("class", "tooltip")
+        .attr(
+          "transform",
+          `translate(${xScale(i) + xScale.bandwidth() + 4}, ${-offset})`
+        )
+        .style("pointer-events", "none")
+        .style("font-size", "11px")
+        .style("font-family", "sans-serif")
+        .style("dominant-baseline", "hanging");
 
-  //     tooltipGroup
-  //       .append("text")
-  //       .attr("dy", 12)
-  //       .text(`${d.anomaly.toFixed(2)}°C`);
-  //   });
+      tooltipGroup
+        .style("opacity", 0)
+        .transition()
+        .duration(250)
+        .style("opacity", 1);
 
-  //   // add a mouseout to transition the rect back to its original position and remove the text
-  //   rect.on("mouseout", function (event) {
-  //     d3.select(this).transition().attr("y", 0).attr("stroke", "none");
-  //     svg.selectAll("g.tooltip").remove();
-  //   });
-  // });
+      tooltipGroup.append("text").text(`${d.year}.`);
+      tooltipGroup
+        .append("text")
+        .attr("dy", 12)
+        .text(`${d.anomaly.toFixed(2)}°C`);
+    });
 
-  // //   let's add x axis to the bottom:
-  // const xAxis = d3
-  //   .axisBottom(xScale)
-  //   // .tickValues(xScale.domain().filter((d, i) => i % 20 === 0))
-  //   // .tickFormat((i) => data[i].year);
+    rect.on("mouseout", function (event) {
+      d3.select(this).transition().attr("y", 0).attr("stroke", "none");
+      svg.selectAll("g.tooltip").remove();
+    });
+  });
 
-  // svg
-  //   .append("g")
-  //   .attr("transform", `translate(0, ${height})`)
-  //   .call(xAxis)
-  //   // .call((g) => g.selectAll(".domain").remove());
+  //   let's add x axis to the bottom:
+  const xAxis = d3
+    .axisBottom(xScale)
+    .tickValues(xScale.domain().filter((d, i) => i % 20 === 0))
+    .tickFormat((i) => data[i].year);
+
+  const axisGroup = svg
+    .append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis)
+    .call((g) => g.selectAll(".domain").remove())
+    .attr("opacity", 0)
+    .transition()
+    .duration(1000)
+    .attr("opacity", 1);
 }
