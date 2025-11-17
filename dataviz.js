748ec5a2e69877cccb1ede85c4a7f75d4f898edf
Veli Gasparovic <velimir.gasparovic@gmail.com>
2025-11-17 09:34:20 +0100

    warming stripes uncommented

--- dataviz.js
+++ dataviz.js
@@ -21,7 +21,7 @@ await d3.json("world_annual.json").then(async (data) => {
 
   console.log(data);
 
-  // await render(data);
+  await render(data);
 });
 
 async function render(data) {
@@ -35,111 +35,111 @@ async function render(data) {
   console.log(xScale.domain().length);
 
   // rects (temporary):
-  data.forEach((d, i) => {
-    svg
-      .append("rect")
-      .attr("x", xScale(i))
-      .attr("y", 0)
-      .attr("width", xScale.bandwidth())
-      .attr("fill", "white")
-      .attr("height", height)
-      .attr("stroke", "black");
-  });
-
-  //diverging color schemes:
-  //https://d3js.org/d3-scale-chromatic/diverging
-  //https://d3js.org/d3-scale/diverging
-
-  // console.log(d3.extent(data, (d) => d.anomaly));
-
-  // const maxAnomaly = d3.max(data, (d) => Math.abs(d.anomaly));
-
-  // // let's contruct a color scale for the rectangles, divergent (blue to red, white in the middle)
-  // const colorScale = d3.scaleDiverging(
-  //   [maxAnomaly, 0, -maxAnomaly],
-  //   d3.interpolateRdBu
-  //   // d3.interpolatePiYG
-  // );
-
-  // // let's iterate data and drawing full heigh rectangles:
-  // const rectGroup = svg.append("g");
-
-  // const rects = data.map((d, i) => {
-  //   return rectGroup
+  // data.forEach((d, i) => {
+  //   svg
   //     .append("rect")
   //     .attr("x", xScale(i))
   //     .attr("y", 0)
   //     .attr("width", xScale.bandwidth())
   //     .attr("fill", "white")
-  //     .attr("height", height);
+  //     .attr("height", height)
+  //     .attr("stroke", "black");
   // });
 
-  // // Create and await all transitions
-  // await Promise.all(
-  //   rects.map((rect, i) => {
-  //     const t = rect
-  //       .transition()
-  //       .duration(2000)
-  //       // .delay(20 * i)
-  //       .attr("fill", colorScale(data[i].anomaly));
+  //diverging color schemes:
+  //https://d3js.org/d3-scale-chromatic/diverging
+  //https://d3js.org/d3-scale/diverging
 
-  //     return t.end();
-  //   })
-  // );
+  console.log(d3.extent(data, (d) => d.anomaly));
+
+  const maxAnomaly = d3.max(data, (d) => Math.abs(d.anomaly));
+
+  // let's contruct a color scale for the rectangles, divergent (blue to red, white in the middle)
+  const colorScale = d3.scaleDiverging(
+    [maxAnomaly, 0, -maxAnomaly],
+    d3.interpolateRdBu
+    // d3.interpolatePiYG
+  );
+
+  // let's iterate data and drawing full heigh rectangles:
+  const rectGroup = svg.append("g");
+
+  const rects = data.map((d, i) => {
+    return rectGroup
+      .append("rect")
+      .attr("x", xScale(i))
+      .attr("y", 0)
+      .attr("width", xScale.bandwidth())
+      .attr("fill", "white")
+      .attr("height", height);
+  });
+
+  // Create and await all transitions
+  await Promise.all(
+    rects.map((rect, i) => {
+      const t = rect
+        .transition()
+        .duration(2000)
+        // .delay(20 * i)
+        .attr("fill", colorScale(data[i].anomaly));
+
+      return t.end();
+    })
+  );
 
   // After all transitions complete, add the interactions
-  // rects.forEach((rect, i) => {
-  //   const d = data[i];
-  //   const offset = 30;
+  rects.forEach((rect, i) => {
+    const d = data[i];
+    const offset = 30;
 
-  //   rect.on("mouseover", () => {
-  //     rect.transition().duration(250).attr("y", -offset);
-  //     rect.attr("stroke", "black").attr("stroke-width", 1).raise();
+    rect.on("mouseover", () => {
+      rect.transition().duration(250).attr("y", -offset);
+      rect.attr("stroke", "black").attr("stroke-width", 1).raise();
 
-  //     const tooltipGroup = svg
-  //       .append("g")
-  //       .attr("class", "tooltip")
-  //       .attr(
-  //         "transform",
-  //         `translate(${xScale(i) + xScale.bandwidth() + 4}, ${-offset})`
-  //       )
-  //       .style("pointer-events", "none")
-  //       .style("font-size", "11px")
-  //       .style("font-family", "sans-serif")
-  //       .style("dominant-baseline", "hanging");
+      const tooltipGroup = svg
+        .append("g")
+        .attr("class", "tooltip")
+        .attr(
+          "transform",
+          `translate(${xScale(i) + xScale.bandwidth() + 4}, ${-offset})`
+        )
+        .style("pointer-events", "none")
+        .style("font-size", "11px")
+        .style("font-family", "sans-serif")
+        .style("dominant-baseline", "hanging");
 
-  //     tooltipGroup
-  //       .style("opacity", 0)
-  //       .transition()
-  //       .duration(250)
-  //       .style("opacity", 1);
+      tooltipGroup
+        .style("opacity", 0)
+        .transition()
+        .duration(250)
+        .style("opacity", 1);
 
-  //     tooltipGroup.append("text").text(`${d.year}.`);
-  //     tooltipGroup
-  //       .append("text")
-  //       .attr("dy", 12)
-  //       .text(`${d3.format("+.2f")(d.anomaly)} °C`);
-  //   });
+      tooltipGroup.append("text").text(`${d.year}.`);
+      tooltipGroup
+        .append("text")
+        .attr("dy", 12)
+        .text(`${d3.format("+.2f")(d.anomaly)} °C`);
+    });
 
-  //   rect.on("mouseout", function (event) {
-  //     d3.select(this).transition().attr("y", 0).attr("stroke", "none");
-  //     svg.selectAll("g.tooltip").remove();
-  //   });
-  // });
+    rect.on("mouseout", function (event) {
+      d3.select(this).transition().attr("y", 0).attr("stroke", "none");
+      svg.selectAll("g.tooltip").remove();
+    });
+  });
 
-  // //   let's add x axis to the bottom:
-  // const xAxis = d3
-  //   .axisBottom(xScale)
-  //   .tickValues(xScale.domain().filter((d, i) => i % 20 === 0))
-  //   .tickFormat((i) => data[i].year);
+  //   let's add x axis to the bottom:
+  const xAxis = d3
+    .axisBottom(xScale)
+    .tickValues(xScale.domain().filter((d, i) => i % 20 === 0))
+    .tickFormat((i) => data[i].year);
 
-  // const axisGroup = svg
-  //   .append("g")
-  //   .attr("transform", `translate(0, ${height})`)
-  //   .call(xAxis)
-  //   .call((g) => g.selectAll(".domain").remove())
-  //   .attr("opacity", 0)
-  //   .transition()
-  //   .duration(1000)
-  //   .attr("opacity", 1);
+  const axisGroup = svg
+    .append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis)
+    .call((g) => g.selectAll(".domain").remove())
+    .attr("opacity", 0)
+    .transition()
+    .duration(1000)
+    .attr("opacity", 1);
 }
